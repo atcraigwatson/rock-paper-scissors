dcc0cfa8b4d891ba8b5a7f075a5fd94d6f20e462
Craig Waton <craig@watsonmemorials.co.uk>
2024-12-15 15:32:24 +0000

    Better code documentation

--- src/components/ChoiceButton.tsx
+++ src/components/ChoiceButton.tsx
@@ -1,6 +1,9 @@
+// Import context
 import { useContext } from "react";
 import Context from "../context";
 
+// Create props for naming the button and passing the name
+// to setPlayerChoice
 interface Props {
   name: string;
 }
@@ -13,13 +16,24 @@ export let trackPlayersChoice = "";
 // and passing props to give the button name and set the playerChoice
 // <ChoiceButton name="Rock" /> would create the rock button.
 const ChoiceButton = ({ name }: Props) => {
+  // Get the necessary state for the component
   const { setPlayerChoice, setCanPlayGame } = useContext(Context);
+
+  // Create the handle click function
   const handleClick = () => {
+    // Sets the trackPlayerChoice variable to the name of the button
+    // being clicked.
     trackPlayersChoice = name;
+    // Sets the playerChoice state to trackPlayerChoice
     setPlayerChoice(trackPlayersChoice);
+    // Sets canPlayGame state to true with enables the play button
+    // by setting the disable attr to false
     setCanPlayGame(true);
+    // Log the choice to the console so the game can be tracked their to
     console.log(`Player chose ${trackPlayersChoice}`);
   };
+  // Return the button element with classes from tailwind and pass name
+  // as the button text.
   return (
     <button
       onClick={handleClick}
