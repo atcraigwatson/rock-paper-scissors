c427ae8b041dc9f3317a21d4e545a4f2c00cd2e2
Craig Waton <craig@watsonmemorials.co.uk>
2024-12-18 19:42:10 +0000

    Comment and Document

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
+// Import context
 import { useContext } from "react";
 import Context from "../context";
 
+// Create Header component
 const Header = () => {
+  // Get playerScore and compScore from context
   const { playerScore, compScore } = useContext(Context);
+
+  // Return a header element with the player score and the comp score
+  // ?? Can the / should the scores be in their own components ??
   return (
     <header className="p-5 bg-gradient-to-br from-orange-500 to-orange-700 text-white ">
       <div>
