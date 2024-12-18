2a26d85b7194a19f94f43cbc9fc0959def43893f
Craig Waton <craig@watsonmemorials.co.uk>
2024-12-18 19:40:04 +0000

    Comment and Document

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,18 @@
+// Import context
 import { useContext } from "react";
 import Context from "../context";
+
+// Import components
 import ChoiceButton from "./ChoiceButton";
 import PlayButton from "./PlayButton";
 import NextRoundButton from "./NextRoundButton";
 
+// Create Footer component
 const Footer = () => {
+  // Get gameStatus from context
   const { gameStatus } = useContext(Context);
+  // If gameStatus is true, return the footer element
+  // with Rock, Paper, Scissors buttons.
   if (gameStatus) {
     return (
       <footer className="flex">
@@ -15,6 +22,8 @@ const Footer = () => {
         <PlayButton />
       </footer>
     );
+    // If the gameStatus is false, return the next round
+    // button in the footer element.
   } else {
     return (
       <footer className="flex">
